3291f7f2c094e67cdec07041a31a1165cef8e352
voidrizoma <milhadasdepapel@gmail.com>
2022-03-15 17:46:50 -0600

    initial commit

---
+++ main.swift
@@ -0,0 +1,13 @@
+//Declarations
+// variables and constants
+
+let numberOfCats = 2 // We must to use let to storage a constant
+var myAge = 33 // and use var to storage a variable
+
+//Type anotation
+
+//"A type annotation explicitly specifies the type of a variable or expression.
+
+var year : Int = 2022
+var isSummer : Bool = false
+
